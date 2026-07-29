e3533e782f6abfe63cf60dac6aab3366ad32a21a
Bror Torstensson <Bror.torstensson@gmail.com>
2026-07-29 21:57:14 +0300

    Fix scrollable point history

--- padelpoints/sw.js
+++ padelpoints/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'padelpoints-v2';
+const CACHE = 'padelpoints-v3';
 const ASSETS = ['./','index.html','styles.css','app.js','manifest.webmanifest','icon.svg'];
 self.addEventListener('install', event => {
   self.skipWaiting();
